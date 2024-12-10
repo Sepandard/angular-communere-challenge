785ed2c5361c3bbfab59eb4a844d6f6bbeab2168
Sepandard <sepandar.derakhshandeh@gmail.com>
2024-12-10 22:10:40 +0330

    refactor(core): add InjectionToken for environment config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LayoutComponent } from './core';
+import { LayoutComponent, ENVIRONMENT_TOKEN } from '@app/core';
+import { ENVIRONMENT } from './environments/environments';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, LayoutComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [
+    {
+      provide: ENVIRONMENT_TOKEN,
+      useValue: ENVIRONMENT, 
+    },
+  ],
 })
 export class AppComponent {
 }
--- src/app/core/components/layout/layout.component.ts
+++ src/app/core/components/layout/layout.component.ts
@@ -11,13 +11,13 @@ import { Title } from '@angular/platform-browser';
 import { Router, RouterModule } from '@angular/router';
 import { AsyncPipe, NgClass, NgFor, NgIf } from '@angular/common';
 import { NAVIGATION_CONFIG, NavigationConfig } from '../../configs/navigation.config';
-import { ENVIRONMENT } from 'src/app/environments/environments';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { HeaderComponent } from '../header/header.component';
 import { MatIconModule } from '@angular/material/icon';
+import { ENVIRONMENT_TOKEN } from '../../token';
 
 @Component({
   selector: 'app-layout',
@@ -43,6 +43,7 @@ export class LayoutComponent {
   public readonly responsiveService = inject(ResponsiveLayoutService);
   private readonly title = inject(Title);
   private readonly router = inject(Router);
+  private readonly environment = inject(ENVIRONMENT_TOKEN);
   private readonly parentInstance = inject(LayoutComponent, { optional: true, skipSelf: true });
   public navList = NAVIGATION_CONFIG;
 
@@ -61,6 +62,6 @@ export class LayoutComponent {
   }
 
   getProjectName() {
-    return ENVIRONMENT.projectName;
+    return this.environment.projectName;
   }
 }
